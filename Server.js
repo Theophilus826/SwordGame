5661b66d75c5b28a6b487b630d3ed309d0238839
Theophilus826 <theophilustelecom@gmail.com>
2026-03-03 13:56:48 +0000

    Update Server.js

--- Server.js
+++ Server.js
@@ -80,7 +80,7 @@ adminNamespace.use((socket, next) => {
 
 adminNamespace.on("connection", (socket) => {
   console.log(`🖥 Admin ${socket.user.name} connected`);
-    registerGameSockets(io, adminNamespace, socket);
+    // registerGameSockets(io, adminNamespace, socket);
     socket.on("admin:getUsers", async () => {
     try {
       const users = await getUsersFromDB();
@@ -148,7 +148,7 @@ io.on("connection", async (socket) => {
       timestamp: Date.now(),
     });
 
-    registerGameSockets(io,adminNamespace, socket);
+     registerGameSockets(io,adminNamespace, socket);
   } catch (err) {
     console.error("Error during connection setup:", err);
   }
@@ -184,6 +184,7 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`.cyan.bold);
 });
+
 
 
 

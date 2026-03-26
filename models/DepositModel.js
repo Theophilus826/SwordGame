30beca9d520e91c24c94bb4ed15fa2403d5a1e46
Theophilus826 <theophilustelecom@gmail.com>
2026-03-26 15:19:22 +0000

    update Deposit

--- models/DepositModel.js
+++ models/DepositModel.js
@@ -1,15 +1,29 @@
-// models/Deposit.js
 const mongoose = require("mongoose");
 
 const depositSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    accountNumber: { type: String, required: true },
-    bankName: { type: String, required: true },
-    accountName: { type: String, required: true },
-    amount: { type: Number, required: true },
-    method: { type: String, enum: ["ngn", "paga", "palmpay"], required: true },
-    status: { type: String, enum: ["PENDING", "COMPLETED", "FAILED"], default: "PENDING" },
-    reference: { type: String }, // optional for verification
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
+  accountNumber: { type: String, required: true },
+  bankName: { type: String, required: true },
+  accountName: { type: String, required: true },
+
+  amount: { type: Number, default: 0 },
+
+  method: { 
+    type: String, 
+    enum: ["ngn"], 
+    default: "ngn" 
+  },
+
+  status: { 
+    type: String, 
+    enum: ["PENDING", "COMPLETED", "FAILED"], 
+    default: "PENDING" 
+  },
+
+  reference: { type: String, required: true, unique: true },
+  paymentReference: { type: String },
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("Deposit", depositSchema);

4ce5903767effb12a4b66e60951695d31a251116
Theophilus826 <theophilustelecom@gmail.com>
2026-03-28 15:57:10 +0000

    Update Server.js

--- Server.js
+++ Server.js
@@ -138,7 +138,7 @@ app.get("/", (req, res) => {
 app.use(express.static(path.join(__dirname, "build"))); // serve static React files
 
 // Catch-all: send index.html for React Router
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 

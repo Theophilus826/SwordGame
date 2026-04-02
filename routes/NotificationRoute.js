538155e94d21389a82b7f04e2a1397e603a24fb0
Theophilus826 <theophilustelecom@gmail.com>
2026-04-02 16:02:36 +0100

    Add deleteNotification route to NotificationRoute

--- routes/NotificationRoute.js
+++ routes/NotificationRoute.js
@@ -6,12 +6,12 @@ const {
   sendNotification,
   sendNotificationToAll,
   getUserNotifications,
-  markAsRead
+  markAsRead, deleteNotification
 } = require("../controller/NotificationController");
 
 router.get("/", protect, getUserNotifications);
 router.post("/", protect, sendNotification);
 router.post("/broadcast", protect, sendNotificationToAll);
 router.put("/:id/read", protect, markAsRead);
-
+router.delete("/:id", protect, deleteNotification);
 module.exports = router;

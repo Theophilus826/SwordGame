f0e2c360f906516dde92d52119de3e4db6638b08
Theophilus826 <theophilustelecom@gmail.com>
2026-04-09 16:03:46 +0100

    Add route to fetch all users
    Added a route to get all users.

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -17,7 +17,7 @@ const {
   sendMood,
   updateAvatar,
   sendMessage,
-  getMessages,
+  getMessages, getAllUsers,
 } = require("../controller/UserController");
 
 const { protect } = require("../middleware/AuthMiddleware");
@@ -73,7 +73,7 @@ router.get("/:userId/posts", protect, async (req, res) => {
     res.status(500).json({ success: false, message: "Failed to fetch user posts" });
   }
 });
-
+router.get("/users", protect, getAllUsers);
 // ==========================
 // CHAT ROUTES
 // ==========================

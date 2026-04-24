d0cc46f8395739fc70a6612a0483e36afb93568e
Theophilus826 <theophilustelecom@gmail.com>
2026-04-24 16:52:31 +0100

    Refactor User model export statement

--- models/UserModels.js
+++ models/UserModels.js
@@ -71,5 +71,4 @@ const userSchema = mongoose.Schema(
   }
 );
 
-module.exports =
-  mongoose.models.User || mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);

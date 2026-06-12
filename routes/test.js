a778d503f3216d51e5ac62c897eb3b56afddde43
Theophilus826 <theophilustelecom@gmail.com>
2026-06-12 23:24:44 +0100

    Update test.js

--- routes/test.js
+++ routes/test.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const admin = require("../config/firebase"); // your admin file
 
-router.get("/test-fcm", async (req, res) => {
+router.get("/test-push", async (req, res) => {
   try {
     const result = await admin.messaging().send({
       token: "eca0upjASfSoQc5we4_NdP:APA91bFoLRSPvoYWrTkC46PewpC4xAbErnkJXnGJhfKxWp8Bb7UV6vlQYbezkOxqeSMHfHAyMN06hh4IJpoOM3lg7MjOyIfULS9nm5XdIl_CLJ3S2alosWU",

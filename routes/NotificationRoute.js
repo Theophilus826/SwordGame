50477048edb960e562ac84de474efddef57c84bd
Theophilus826 <theophilustelecom@gmail.com>
2026-06-20 16:34:34 +0100

    Delete test push notification route
    Removed test push notification route and associated code.

--- routes/NotificationRoute.js
+++ routes/NotificationRoute.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/AuthMiddleware");
-const User = require("../models/UserModels");
-const admin = require("../config/firebase");
+
 const {
   sendNotification,
   sendNotificationToAll,
@@ -42,45 +41,5 @@ router.delete("/:id", protect, deleteNotification);
 // Save FCM token
 router.post("/fcm-token", protect, saveFcmToken);
 
-router.post("/test-push", protect, async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id).select("fcmToken");
-
-    console.log("👤 USER:", user._id);
-    console.log("📱 TOKEN:", user.fcmToken);
-
-    if (!user.fcmToken) {
-      return res.status(400).json({
-        success: false,
-        message: "No FCM token found",
-      });
-    }
-
-    const result = await admin.messaging().send({
-      token: user.fcmToken,
-
-      notification: {
-        title: "Test Notification (API)",
-        body: "If you see this, route works",
-      },
-
-      android: {
-        priority: "high",
-      },
-    });
-
-    res.json({
-      success: true,
-      result,
-    });
-  } catch (err) {
-    console.error("TEST PUSH ERROR:", err);
-
-    res.status(500).json({
-      success: false,
-      error: err.message,
-    });
-  }
-});
 
 module.exports = router;

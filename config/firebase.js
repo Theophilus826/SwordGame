fa1c34f301feb33c250d5f996fda608f9d824fb0
Theophilus826 <theophilustelecom@gmail.com>
2026-06-23 20:45:56 +0100

    Add logging for Firebase init and server time
    Added logging for Firebase initialization and server time.

--- config/firebase.js
+++ config/firebase.js
@@ -2,6 +2,10 @@ const admin = require("firebase-admin");
 
 const serviceAccount = require("../tinkreward-firebase-adminsdk-fbsvc-54d4ffb421.json");
 
+console.log("FIREBASE INIT TIME:");
+console.log("SERVER TIME:", new Date().toISOString());
+console.log("EPOCH:", Date.now());
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });

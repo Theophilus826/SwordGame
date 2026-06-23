8f8226a3af31ecf2f5bb0f75981a0fc59009aaad
Theophilus826 <theophilustelecom@gmail.com>
2026-06-23 19:50:48 +0100

    Update Firebase service account JSON file path

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../tinkreward-firebase-adminsdk-fbsvc-d783b46028.json");
+const serviceAccount = require("../tinkreward-firebase-adminsdk-fbsvc-54d4ffb421.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

60711f00c08ea79387f8504b0d72796e3461181c
Theophilus826 <theophilustelecom@gmail.com>
2026-06-24 15:36:07 +0100

    Update Firebase initialization to use environment variables

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,9 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../tinkreward-firebase-adminsdk-fbsvc-54d4ffb421.json");
-
-console.log("FIREBASE INIT TIME:");
-console.log("SERVER TIME:", new Date().toISOString());
-console.log("EPOCH:", Date.now());
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
-
-module.exports = admin;

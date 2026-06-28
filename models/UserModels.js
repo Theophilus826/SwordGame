2291c7aae56386fd2060a8d5b9eacc77ba1faa48
Theophilus826 <theophilustelecom@gmail.com>
2026-06-28 17:55:54 +0100

    Add phone verification fields to user model
    Added phone verification fields to user schema.

--- models/UserModels.js
+++ models/UserModels.js
@@ -48,6 +48,8 @@ const userSchema = new mongoose.Schema(
 
     resetPasswordToken: String,
     resetPasswordExpire: Date,
+    phoneVerificationToken: String,
+    phoneVerificationExpire: Date,
   },
   { timestamps: true },
 );

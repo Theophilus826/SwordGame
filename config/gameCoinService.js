ebbfe0a41a61cec99f5ee01be64d73756a82c5d9
Theophilus826 <theophilustelecom@gmail.com>
2026-07-20 00:22:49 +0100

    Refactor processGameCoins to simplify logic

--- config/gameCoinService.js
+++ config/gameCoinService.js
@@ -3,12 +3,7 @@
 const User = require("../models/UserModels");
 const { updateCoins } = require("../controller/AccountController");
 
-async function processGameCoins({
-  gameId,
-  action,
-  amount = 0,
-  playerId,
-}) {
+async function processGameCoins({ gameId, action, amount = 0, playerId }) {
   const admin = await User.findOne({ isAdmin: true });
 
   if (!admin) {
@@ -49,12 +44,11 @@ async function processGameCoins({
         throw new Error("Missing playerId for PLAYER_WIN");
       }
 
-      // Debit admin
-      await updateCoins({
-        userId: admin._id,
-        amount: -amount,
-        type: "GAME_PAYOUT",
-        description: `Paid winner for game ${gameId}`,
+      return updateCoins({
+        userId: playerId,
+        amount,
+        type: "GAME_WIN",
+        description: `Won game ${gameId}`,
       });
 
       // Credit player
@@ -69,8 +63,8 @@ async function processGameCoins({
       return updateCoins({
         userId: admin._id,
         amount,
-        type: "GAME_RETURN",
-        description: `Player lost game ${gameId}`,
+        type: "GAME_WIN",
+        description: `Won game ${gameId}`,
       });
 
     case "GAME_CANCELLED":
